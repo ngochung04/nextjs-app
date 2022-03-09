01a92c1f243c24c33887b24ca8b3d82e212840a0
ngochung04 <dangngochung0401@gmail.com>
2022-03-09 15:18:34 +0700

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -18,38 +19,52 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <div className={styles.card}>
+            <h2>Link &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <li>
+                <Link href="/link" passHref>
+                  <a>Link</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/link/PageOne" passHref>
+                  <a>PageOne</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/link/PageTwo" passHref>
+                  <a>PageTwo</a>
+                </Link>
+              </li>
             </p>
-          </a>
+          </div>
+
+          <div className={styles.card}>
+            <h2>Routing &rarr;</h2>
+            <p>
+              <li>
+                <Link href="/routing/single/param" passHref>
+                  <a>Single</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/routing/multiple/param1/param2" passHref>
+                  <a>Multiple</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/routing/param0/param1/param2" passHref>
+                  <a>Catch All</a>
+                </Link>
+              </li>
+            </p>
+          </div>
         </div>
       </main>
 
@@ -59,14 +74,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
